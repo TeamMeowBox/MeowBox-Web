0f1b3fd970ea32d8f80863d6605db830e71a7862
NohYujeong <shdbwjd0326@naver.com>
2018-07-01 21:04:56 +0900

    뮤박스 홈 구조화 작업

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,5 +1,124 @@
 <template>
-    <p>home</p>
+    <div>
+        <article class="main">
+            <section class="container" style="background-color:rgba(224, 224, 224, 0.1); text-align:center">
+                <p>집사의 고민을 끝내줄</p>
+                <p>내 고양이 만을 위한 특별한 선물</p>
+                <p>MEOW BOX</p>
+                <v-btn color="error">미유박스 시작</v-btn>
+            </section>
+        </article>
+        <article class="live-react">
+            <section class="container live-react-title" style="text-align:center">
+                <p>이번 달 미유박스는</p>
+                <p>3,800냥이 를 만났어요!</p>
+            </section>
+            <section class="container live-react-instas" style="text-align:center">
+                <p>여기에 인스타 크롤링 리스트</p>
+                <v-layout>
+                    <v-flex xs12 sm6 md4 lg3>
+                    <v-card>
+                        <v-card-title primary-title>
+                            <div>
+                                <div>Insta Profile</div>
+                            </div>
+                        </v-card-title>
+                        <v-card-media
+                        src="/doc-images/cards/desert.jpg"
+                        height="200px"
+                        ></v-card-media>
+                        <v-card-actions>
+                        <v-btn flat color="pink"><i class="fas fa-heart"></i></v-btn>
+                        <v-btn flat color="pink"><i class="fas fa-comment"></i></v-btn>
+                        <v-btn flat color="pink"><i class="fas fa-bookmark"></i></v-btn>
+                        </v-card-actions>
+                    </v-card>
+                    </v-flex>
+                    <v-flex xs12 sm6 md4 lg3>
+                    <v-card>
+                        <v-card-title primary-title>
+                            <div>
+                                <div>Insta Profile</div>
+                            </div>
+                        </v-card-title>
+                        <v-card-media
+                        src="/doc-images/cards/desert.jpg"
+                        height="200px"
+                        ></v-card-media>
+                        <v-card-actions>
+                        <v-btn flat color="pink"><i class="fas fa-heart"></i></v-btn>
+                        <v-btn flat color="pink"><i class="fas fa-comment"></i></v-btn>
+                        <v-btn flat color="pink"><i class="fas fa-bookmark"></i></v-btn>
+                        </v-card-actions>
+                    </v-card>
+                    </v-flex>
+                    <v-flex xs12 sm6 md4 lg3>
+                    <v-card>
+                        <v-card-title primary-title>
+                            <div>
+                                <div>Insta Profile</div>
+                            </div>
+                        </v-card-title>
+                        <v-card-media
+                        src="/doc-images/cards/desert.jpg"
+                        height="200px"
+                        ></v-card-media>
+                        <v-card-actions>
+                        <v-btn flat color="pink"><i class="fas fa-heart"></i></v-btn>
+                        <v-btn flat color="pink"><i class="fas fa-comment"></i></v-btn>
+                        <v-btn flat color="pink"><i class="fas fa-bookmark"></i></v-btn>
+                        </v-card-actions>
+                    </v-card>
+                    </v-flex>
+                    <v-flex xs12 sm6 md4 lg3>
+                    <v-card>
+                        <v-card-title primary-title>
+                            <div>
+                                <div>Insta Profile</div>
+                            </div>
+                        </v-card-title>
+                        <v-card-media
+                        src="/doc-images/cards/desert.jpg"
+                        height="200px"
+                        ></v-card-media>
+                        <v-card-actions>
+                        <v-btn flat color="pink"><i class="fas fa-heart"></i></v-btn>
+                        <v-btn flat color="pink"><i class="fas fa-comment"></i></v-btn>
+                        <v-btn flat color="pink"><i class="fas fa-bookmark"></i></v-btn>
+                        </v-card-actions>
+                    </v-card>
+                    </v-flex>
+                </v-layout>
+
+            </section>
+        </article>
+        <article class="container what-is-meowbox">
+            <section class="what-is-title">
+                <h2>
+                    미유박스란?
+                </h2>
+            </section>
+            <section class="what-is-content">
+                <p>미유박스는 고양이에게 필요한 용품과 간식을 담은 박스를 정기적으로 <br>배송해주는 서비스입니다.<br>나의 고양이를 생각하는 모든 집사들의 마음을 담은 박스입니다.</p>
+            </section>
+        </article>
+        <article class="container this-month-meowbox-info" style="text-align:right;">
+             <section class="this-month-meowbox-title">
+                <h2>
+                    이번 달 미유박스는?
+                </h2>
+            </section>
+            <section class="this-month-meowbox-content">
+                <p>시원한 바다같은, 7월의 박스! <br>'under the sea' 패키지를 만나보세요!</p>
+                <v-btn color="error">미유박스 상세보기</v-btn>
+            </section>
+        </article>
+        <article class="footer-start">
+            <section class="container" style="text-align:center">
+                <v-btn color="error">미유박스 시작하기</v-btn>
+            </section>
+        </article>
+    </div>
 </template>
 
 <script>

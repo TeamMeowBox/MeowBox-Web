fc4dd44748a5bff52d081f13b74c6406484e2f22
ManHyuk <manhyuk@kw.ac.kr>
2018-07-08 01:56:26 +0900

    add suggestion

--- MeowBox-Web/src/components/MyPage/MySuggestion.vue
+++ MeowBox-Web/src/components/MyPage/MySuggestion.vue
@@ -13,7 +13,7 @@
                     </td>
                     <td class="suggestTd2" >
                         <label for="suggestTitle" class="titleLabel">제목을 입력해주세요</label>
-                        <v-text-field class="suggestTitle" type="text" placeholder="어떤 의견인가요?"></v-text-field>
+                        <v-text-field class="suggestTitle" type="text" placeholder="어떤 의견인가요?" v-model="suggestion.title"></v-text-field>
                     </td>
                 </tr>
                 <tr>
@@ -26,22 +26,45 @@
                         solo
                         name="input-7-4"
                         label="Solo textarea"
-                        v-model="etc"
-                        placeholder="미유박스 이용 경험에 대해 알려주세요. 만족스러웠던 점과 개선해야 할 점을 알려주세요. 더 노력하는 미유박스가 될게요."
-                        ></v-textarea>
+                        v-model="suggestion.content"
+                        placeholder="미유박스 이용 경험에 대해 알려주세요. 만족스러웠던 점과 개선해야 할 점을 알려주세요. 더 노력하는 미유박스가 될게요."></v-textarea>
                     </td>
                 </tr>
             </table>
         </section>
         <aside class="suggestBtnSpace">
-                <v-btn class="suggestBtn">제안하기</v-btn>
+                <v-btn class="suggestBtn" @click="clickSuggest">제안하기</v-btn>
         </aside>
     </div>
 </template>
 
 <script>
-export default {
+import axios from 'axios';
 
+export default {
+  name: 'MySuggestion',
+  data() {
+    return {
+      suggestion: {
+        title: null,
+        content: null
+      }
+    }
+  },
+  methods: {
+    async clickSuggest() {
+      this.suggestion.user_idx = localStorage.getItem('user_idx');
+
+      try {
+        const result = await axios.post('http://13.209.220.1:3000/mypage/feedback', this.suggestion, {headers: {authorization: localStorage.getItem('token')}});
+        if (result.data.status) {
+          alert('success');
+        }
+      } catch (e) {
+        alert('failed');
+      }
+    }
+  }
 }
 </script>
 

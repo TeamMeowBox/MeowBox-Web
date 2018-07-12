a1a3177a08d1f3898bef8966a4f845248dbe028e
NohYujeong <shdbwjd0326@naver.com>
2018-07-13 01:38:59 +0900

    미유박스 이야기랑 웹툰

--- MeowBox-Web/src/pages/Story.vue
+++ MeowBox-Web/src/pages/Story.vue
@@ -1,21 +1,14 @@
 <template>
-    <div class="container">
+    <div class="container" style="width:70vw">
         <story-banner></story-banner>
-        <what-is-meowbox></what-is-meowbox>
-        <sensitive-cont></sensitive-cont>
     </div>
 </template>
 
 <script>
 import StoryBanner from '../components/Story/StoryBanner'
-import WhatIsMeowbox from '../components/Story/WhatIsMeowbox'
-import SensitiveCont from '../components/Story/SensitiveCont'
-
 export default {
     components: {
-        StoryBanner,
-        WhatIsMeowbox,
-        SensitiveCont
+        StoryBanner
     }    
 }
 </script>

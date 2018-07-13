2aa395a1351d0725fbf32abe059c9d353b988544
우아테크캠프_6 <woowatechcamp_6@woowatechcamp6.local>
2018-07-13 23:01:02 +0900

    고양이 인덱스 수정

--- MeowBox-Web/src/pages/HappyBirthday.vue
+++ MeowBox-Web/src/pages/HappyBirthday.vue
@@ -1,13 +1,29 @@
 <template>
     <div class="container" style="width:70vw; text-align:center">
-        <img src="../assets/images/webtoon_img.png" alt="" style="width:50vw">
+        <birthday-detail v-if="flag === 1"></birthday-detail>
+        <img v-else src="../assets/images/webtoon_img.png" alt="" style="width:50vw">
         <br><br>
-        <v-btn class="birthday-info" router to="/happyBirthday/detail" >생일박스 상세보기</v-btn>
+        <v-btn class="birthday-info" @click="changeFlag()" >생일박스 상세보기</v-btn>
     </div>
 </template>
 
 <script>
+import BirthdayDetail from '../components/Birthday/BirthdayDetail'
 export default {
+    data(){
+        return{
+            flag:-1
+        }
+    },
+components:{
+    BirthdayDetail
+},
+methods:{
+    changeFlag(){
+        this.flag = this.flag*-1;
+    }
+}
+
 
 }
 </script>
